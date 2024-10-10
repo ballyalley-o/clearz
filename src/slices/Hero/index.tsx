fcde2291ecaaab863dce0fd7eb654c796015825a
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-10-11 00:53:12 +1300

    feat: add Hero component for rendering Hero Slices

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,31 @@
+import { Content } from "@prismicio/client";
+import { PrismicNextLink, PrismicNextImage } from "@prismicio/next";
+import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.subheading} />
+      <PrismicRichText field={slice.primary.body} />
+      {slice.primary.button_text}
+      <PrismicNextLink field={slice.primary.button_link} />
+      <PrismicNextImage field={slice.primary.cans_image} />
+      <PrismicRichText field={slice.primary.second_heading} />
+      <PrismicRichText field={slice.primary.second_body} />
+    </section>
+  );
+};
+
+export default Hero;
