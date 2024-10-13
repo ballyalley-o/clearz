9128c852acc1f14648820087df46e7205d80905d
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-10-13 22:36:45 +1300

    feat: export TextSplitter component from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./header/header";
 export { ClearzLogo } from "./brand/clearz-logo";
 export { Bounded } from "./bounded/bounded";
 export { Button } from "./button/button";
+export { TextSplitter } from "./text-splitter/text-splitter";
