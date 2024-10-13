63daa2ef502e5fdf25042dd17b591123d7d394de
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-10-13 22:30:16 +1300

    feat: add Button component for linking with customizable styles

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
-export { default as Header } from './header/header';
-export { ClearzLogo } from './brand/clearz-logo';
-export { Bounded } from './bounded/bounded'
+export { default as Header } from "./header/header";
+export { ClearzLogo } from "./brand/clearz-logo";
+export { Bounded } from "./bounded/bounded";
+export { Button } from "./button/button";
