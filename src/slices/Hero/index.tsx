f969449b040e7328dc778feaa595e38fecbf5db6
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-10-15 00:32:36 +1300

    feat: implement GSAP animations and scroll triggers in Hero component

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Content, asText } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
 import { Bounded, Button, TextSplitter } from "components";
 
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 /**
  * Props for `Hero`.
  */
@@ -11,8 +17,61 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+
+    introTl
+    .set(".hero", {opacity: 1})
+    .from(".hero-header-word", {
+      scale: 3,
+      opacity: 0,
+      ease: "power4.in",
+      delay: .3,
+      stagger: 1
+    })
+    .from(".hero-subheading", {
+      opacity: 0,
+      y: 30,
+    }, "+=.8")
+    .from(".hero-body", {
+      opacity: 0,
+      y: 30,
+    })
+    .from(".hero-button", {
+      opacity: 0,
+      y: 10,
+      duration: .6
+    });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true
+      }
+    });
+
+    scrollTl.fromTo("body", { backgroundColor: "#FDE047"}, { backgroundColor: "#D9F99D", overwrite: "auto" }, 1)
+    .from(".text-side-heading .split-char", {
+      scale: 1.3,
+      y:40,
+      rotate:-25,
+      opacity: 0,
+      stagger: .1,
+      ease: "back.out(3)",
+      duration: .5
+    })
+    .from(".text-side-body", {
+      y: 30,
+      opacity: 0,
+    })
+  })
+
   return (
-    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="hero">
+    <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="hero opacity-0">
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
