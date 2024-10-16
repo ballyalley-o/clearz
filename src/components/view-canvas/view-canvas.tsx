e8401e79d0b8c77fc92cc6c9a3e1febbfd0f3e81
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-10-16 23:41:48 +1300

    feat: update ViewCanvas component to render a 3D box using React Three Fiber

--- src/components/view-canvas/view-canvas.tsx
+++ src/components/view-canvas/view-canvas.tsx
@@ -4,5 +4,12 @@ import { Canvas } from "@react-three/fiber";
 type Props = {};
 
 export default function ViewCanvas({}: Props) {
-  return <div>ViewCanvas</div>;
+  return (
+    <Canvas>
+      <mesh>
+        <boxBufferGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="" />
+      </mesh>
+    </Canvas>
+  );
 }
