3653cd851a1aa34488fa4dcfcfbcefecc8ff5941
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-10-20 01:58:16 +1300

    feat: update ViewCanvas component with fixed positioning and enhanced lighting for improved 3D rendering

--- src/components/view-canvas/view-canvas.tsx
+++ src/components/view-canvas/view-canvas.tsx
@@ -1,15 +1,27 @@
 "use client";
+
 import { Canvas } from "@react-three/fiber";
 
 type Props = {};
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas>
-      <mesh>
-        <boxBufferGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="" />
+    <Canvas
+      style={{
+        position: "fixed",
+        top: 0,
+        left: "50%",
+        transform: "translateX(-50%)",
+        overflow: "hidden",
+        pointerEvents: "none",
+        zIndex: 30,
+      }}
+    >
+      <mesh rotation={[0.5, 0.5, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color={"hotpink"} />
       </mesh>
+      <ambientLight intensity={2} />
     </Canvas>
   );
 }
