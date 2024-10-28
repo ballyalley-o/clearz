f2ec3443a1076fc69f8f817994054d1698261225
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-10-29 02:27:43 +1300

    feat: add ClearzCan component and Floating animation export

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,9 @@ export { ClearzLogo } from "./brand/clearz-logo";
 export { Bounded } from "./bounded/bounded";
 export { Button } from "./button/button";
 export { TextSplitter } from "./text-splitter/text-splitter";
+
+// clearz-can
+export { ClearzCan } from "./clearz-can/clearz-can";
+export type { ClearzCanProps } from "./clearz-can/clearz-can";
+// animate
+export { default as Floating } from "./animate/floating";
