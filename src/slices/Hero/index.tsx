3133dc5f64fc044a135e1066f40f2fdf014e98df
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-10-29 02:27:53 +1300

    feat: integrate Scene component into Hero for enhanced visuals

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -5,8 +5,10 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { Content, asText } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
+import { View } from "@react-three/drei";
 import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
 import { Bounded, Button, TextSplitter } from "components";
+import Scene from "./scene";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 /**
@@ -76,6 +78,9 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
 
   return (
     <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="hero opacity-0">
+      <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
+        <Scene />
+      </View>
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
