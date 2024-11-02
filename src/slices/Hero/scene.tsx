174b44a36adfb7c8972c6744010b5b2b0ae4c814
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-11-02 23:47:51 +1300

    feat: enhance Scene component with floating cans and GSAP animations

--- src/slices/Hero/scene.tsx
+++ src/slices/Hero/scene.tsx
@@ -1,14 +1,65 @@
 "use client";
 
+import { useRef } from "react";
+import { Group } from "three";
 import { Environment } from "@react-three/drei";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { Floating } from "components";
+
+gsap.registerPlugin(useGSAP);
 
 type Props = {};
 
 export default function Scene({}: Props) {
+  const can1Ref = useRef<Group>(null);
+  const can2Ref = useRef<Group>(null);
+  const can3Ref = useRef<Group>(null);
+  const can4Ref = useRef<Group>(null);
+  const can5Ref = useRef<Group>(null);
+
+  const can1GroupRef = useRef<Group>(null);
+  const can2GroupRef = useRef<Group>(null);
+
+  const groupRef = useRef<Group>(null);
+
+  const FLOAT_SPEED = 1.5;
+
+  useGSAP(() => {
+    if (
+      !can1Ref.current ||
+      !can2Ref.current ||
+      !can3Ref.current ||
+      !can4Ref.current ||
+      !can5Ref.current ||
+      !can1GroupRef.current ||
+      !can2GroupRef.current ||
+      !groupRef.current
+    )
+      return;
+
+    gsap.set(can1Ref.current.position, { x: -1.5 });
+    gsap.set(can1Ref.current.position, { z: -0.5 });
+
+    gsap.set(can2Ref.current.position, { x: 1.5 });
+    gsap.set(can2Ref.current.position, { z: 0.5 });
+
+    gsap.set(can3Ref.current.position, { y: 5, z: 2 });
+    gsap.set(can4Ref.current.position, { x: 2, y: 4, z: 2 });
+    gsap.set(can5Ref.current.position, { y: -5 });
+  });
+
   return (
-    <group>
-      <Floating />
+    <group ref={groupRef}>
+      <group ref={can1GroupRef}>
+        <Floating ref={can1Ref} flavor="blackCherry" floatSpeed={FLOAT_SPEED} />
+      </group>
+      <group ref={can2GroupRef}>
+        <Floating ref={can2Ref} flavor="lemonLime" floatSpeed={FLOAT_SPEED} />
+      </group>
+      <Floating ref={can3Ref} flavor="grape" floatSpeed={FLOAT_SPEED} />
+      <Floating ref={can4Ref} flavor="strawberryLemonade" floatSpeed={FLOAT_SPEED} />
+      <Floating ref={can5Ref} flavor="watermelon" floatSpeed={FLOAT_SPEED} />
       <Environment files="/hdr/lobby.hdr" environmentIntensity={1.5} />
     </group>
   );
