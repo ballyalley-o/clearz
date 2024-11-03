7cda316e7877183a396806eed1b46e549c8806e3
Bally.Lomibao <bally.lomibao@howickltd.com>
2024-11-03 21:17:28 +1300

    feat: add Bubble component to Hero section for animated floating effects

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -8,6 +8,7 @@ import { PrismicNextImage } from "@prismicio/next";
 import { View } from "@react-three/drei";
 import { SliceComponentProps, PrismicRichText } from "@prismicio/react";
 import { Bounded, Button, TextSplitter } from "components";
+import { Bubble } from "./bubble";
 import Scene from "./scene";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
@@ -80,6 +81,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <Bounded data-slice-type={slice.slice_type} data-slice-variation={slice.variation} className="hero opacity-0">
       <View className="hero-scene pointer-events-none sticky top-0 z-50 -mt-[100vh] hidden h-screen w-screen md:block">
         <Scene />
+        <Bubble count={300} speed={2} repeat={true} />
       </View>
       <div className="grid">
         <div className="grid h-screen place-items-center">
